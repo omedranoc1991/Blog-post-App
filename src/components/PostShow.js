5fd16c289b750f2a31db5fe8e2c6ad6647b2393a
Oscar Medrano Castillo <omedranoc@hotmail.com>
2020-09-01 19:26:45 -0500

    I added the las part of the project

---
+++ src/components/PostShow.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+import {fetchPost, deletePost} from '../actions'
+
+class PostShow extends React.Component{
+
+    componentDidMount(){
+
+        if(!this.props.post){
+            const {id}=this.props.match.params
+            this.props.fetchPost(id)
+        }    
+    }
+
+    onDeleteClick(){
+        const {id}=this.props.match.params
+        this.props.deletePost(id, ()=>{
+            this.props.history.push('/')
+        })
+    }
+    
+    render(){
+
+        const {post} = this.props
+
+        if(!post){
+            return <div>Loading...</div>
+        }
+
+        return(
+            <div>
+            <Link to='/' >Back to index</Link>
+            <button
+             className='btn btn-danger pull-xs-right'
+             onClick={this.onDeleteClick.bind(this)}
+             >Delete post
+             </button>
+                <h3>{post.title}</h3>
+                <h6>Categories : {post.categories}</h6>
+                <p>{post.content}</p>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = ({posts},ownProps)=>{
+    return { post : posts[ownProps.match.params.id]}
+}
+
+export default connect(mapStateToProps, {fetchPost, deletePost})(PostShow)
--- src/components/PostsNews.js
+++ src/components/PostsNews.js
@@ -1,23 +1,34 @@
 import React from 'react'
 import {Field, reduxForm} from 'redux-form'
+import {Link} from 'react-router-dom'
+import {connect} from 'react-redux'
+import {createPost} from '../actions'
 
 class PostsNews extends React.Component{
 
     renderField(field){
+
+       const className=`form-group ${field.meta.touched && field.meta.error ? 'has-danger' : ''}`
+
        return(
-       <div className='form-group'>
+       <div className={className}>
             <label>{field.label}</label>
             <input
                 className='form-control'
                 type='text'
                 {...field.input}
             />
-            {field.meta.error}
+            <div className='text-help'>
+               {field.meta.touched ? field.meta.error : ''}
+            </div>            
         </div>)
     }
     
     onSubmit(values){
-        console.log(values)
+        
+        this.props.createPost(values, ()=>{
+            this.props.history.push('/')
+        })
     }
 
     render(){
@@ -29,6 +40,7 @@ class PostsNews extends React.Component{
                 <Field label='Categories' name='categories' component={this.renderField}/>
                 <Field label='Post content' name='content' component={this.renderField}/>
                 <button type='submit' className='btn btn-primary'>Submit</button>
+                <Link to='' className='btn btn-danger'>Cancel</Link>
             </form>
         )
     }
@@ -52,4 +64,4 @@ const validate=(values)=>{
     return errors 
 }
 
-export default reduxForm({validate,form : 'postsNewForm'})(PostsNews)
+export default reduxForm({validate,form : 'postsNewForm'})(connect(null,{createPost})(PostsNews))
